89fe913c4e7a84b3f247679b976d55245d0af069
Justin Chen <justin.uob@gmail.com>
2018-10-14 20:15:37 +0800

    第一題完成了

--- src/trendmicro/codility/CodilityTest.java
+++ src/trendmicro/codility/CodilityTest.java
@@ -17,34 +17,22 @@ public class CodilityTest {
 		
 	}
 	
-	public static int solution(int[] A) {
+	public static int solution(int[] T) {
 		
-		int totalCandyNumber = A.length;
-		// System.out.println(totalCandyNumber); // 6
+		// 總糖果數
+		int totalCandyNumber = T.length;
 		
 		// Mary的糖果盒，最多可以裝的糖果數量
 		int marrysCandyBoxMaxNum = totalCandyNumber/2;
-		// System.out.println(marrysCandyBoxMaxNum); // 3個 
 		
 		// Mary的糖果盒
-		//int[] marrysCandyBox = new int[marrysCandyBoxMaxNum];
 		List<Integer> list = new ArrayList<Integer>();
-		list.add(A[0]);
-		
-		
-		// 直接先取第一個糖果，放進來
-		//marrysCandyBox[0] = A[0];
-		//System.out.println(marrysCandyBox[0]);
+		list.add(T[0]);
 		
 		// 之後逐一比對，不含就放進來，放滿為止
-		
-		
-		
-		// go through all the candies
 		for(int i = 1; i<totalCandyNumber; i++) {
-			// System.out.println(A[i]);
-			if(!list.contains(A[i])) {
-				list.add(A[i]);
+			if(!list.contains(T[i])) {
+				list.add(T[i]);
 				if(list.size()==marrysCandyBoxMaxNum) {
 					// 裝滿了就出去
 					return list.size();
@@ -57,33 +45,33 @@ public class CodilityTest {
 		return list.size();
 	}
 	
-	public static boolean contains(int[] arr, int item) {
-		for (int n : arr) {
-			if (item == n) {
-				return true;
-			}
-		}
-		return false;
-	}
+//	public static boolean contains(int[] arr, int item) {
+//		for (int n : arr) {
+//			if (item == n) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
 	
 	
 	
 	
-	public static boolean bGreaterThanA(Integer a, Integer b) {
-//		Integer a = 8;
-//		Integer b = 200;
-		    int compareValue = a.compareTo(b);
-		    if (compareValue > 0) {
-		        System.out.println("a is greater than b");
-		        return false;
-		    } else if (compareValue < 0) {
-		        System.out.println("b is greater than a");
-		        return true;
-		    } else {
-		        System.out.println("a and b are equal");
-		        return false;
-		    }
-	}
+//	public static boolean bGreaterThanA(Integer a, Integer b) {
+////		Integer a = 8;
+////		Integer b = 200;
+//		    int compareValue = a.compareTo(b);
+//		    if (compareValue > 0) {
+//		        System.out.println("a is greater than b");
+//		        return false;
+//		    } else if (compareValue < 0) {
+//		        System.out.println("b is greater than a");
+//		        return true;
+//		    } else {
+//		        System.out.println("a and b are equal");
+//		        return false;
+//		    }
+//	}
 	
 
 }
